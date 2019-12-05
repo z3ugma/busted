f85cb640789f1c4fe1b67423113fae06517f0c5f
Fred Turkington <z3ugma@users.noreply.github.com>
2019-12-05 17:43:07 -0600

    Load dotenv in configuration

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, jsonify, render_template
 import requests, os, json, datetime
 import pyjade
 from flask_assets import Environment, Bundle
+from dotenv import load_dotenv
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.flaskenv')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
